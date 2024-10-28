5ecff234cb6ac1dc964a42f83ba0a2c5b13abbad
Peter Olsen <olse0321@umn.edu>
2024-10-28 14:11:26 -0500

    started svelte demo

---
+++ example/src/routes/test/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    let count = $state(0);
+
+    function increment() {
+        count += 1;
+    }
+</script>
+
+<div>
+    <h1>
+        Hello!
+    </h1>
+
+    <br>
+    <div>
+        Count = {count}
+    </div>
+    <button on:click={increment}>
+        Click me!
+    </button>
+</div>
